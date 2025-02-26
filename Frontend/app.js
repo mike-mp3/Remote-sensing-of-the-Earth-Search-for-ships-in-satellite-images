f8bf1cdcf4911d6ecbdb1e18a932273af0dba476
mike-mp3 <mishamarkov12.0@gmail.com>
2025-02-27 00:29:07 +0300

    сделал так чтобы картинка добавлялась в class="uploaded-image"

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,25 +1,25 @@
-[document.addEventListener](document.addEventListener('DOMContentLoaded'), () => {
-    const form = [document.getElementById](document.getElementById('upload-form'));
-    const fileInput = [document.getElementById](document.getElementById('file-input'));
-    const imagesContainer = [document.querySelector](document.querySelector('.images-container'));
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('upload-form');
+    const fileInput = document.getElementById('file-input');
+    const imagesContainer = document.querySelector('.images-container');
     let uploadedImages = [];
 
-    // Обработчик события отправки формы ToDo
-    [form.addEventListener](form.addEventListener('submit'), (event) => {
-        [event.preventDefault()](event.preventDefault());
+    // Обработчик события отправки формы
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
         if (fileInput.files.length > 0) {
-            for (let i = 0; i < [fileInput.files.length](fileInput.files.length) && i < 3; i++) {
+            for (let i = 0; i < fileInput.files.length && i < 3; i++) { // Ограничение до трех изображений
                 const reader = new FileReader();
-                [reader.onload](reader.onload) = function(event) {
-                    const img = [document.createElement](document.createElement('img'));
-                    [img.src](img.src) = [event.target.result](event.target.result);
-                    [img.classList.add](img.classList.add('uploaded-image'));
-                    [imagesContainer.appendChild(img)](imagesContainer.appendChild(img));
-                    [uploadedImages.push(img)](uploadedImages.push(img));
+                reader.onload = function(event) {
+                    const img = document.createElement('img');
+                    img.src = event.target.result;
+                    img.classList.add('uploaded-image');
+                    imagesContainer.appendChild(img);
+                    uploadedImages.push(img); // Сохраняем ссылку на добавленное изображение
                 };
-                [reader.readAsDataURLfileInput.files](reader.readAsDataURL(fileInput.files)[i]);
+                reader.readAsDataURL(fileInput.files[i]); // Читаем файл
             }
         }
     });
-});
+})
