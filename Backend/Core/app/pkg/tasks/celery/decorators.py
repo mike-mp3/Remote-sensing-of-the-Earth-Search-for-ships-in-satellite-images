cf7723383f3f10a50078514fccd0d2482afd15ef
oguzok2012 <24olegsandr24@gmail.com>
2025-04-27 17:38:50 +0300

    [*] fix: 1 celery worker

--- Backend/Core/app/pkg/tasks/celery/decorators.py
+++ Backend/Core/app/pkg/tasks/celery/decorators.py
@@ -21,15 +21,7 @@ def init_celery_worker(**kwargs):
 def async_to_sync(task: Any) -> Callable[[Any], Any]:
     @wraps(task)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        loop = asyncio.get_event_loop()
-        if loop.is_running():
-            new_loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(new_loop)
-            try:
-                return new_loop.run_until_complete(task(*args, **kwargs))
-            finally:
-                new_loop.close()
-        else:
-            return loop.run_until_complete(task(*args, **kwargs))
-
+        return asyncio.run(
+            task(*args, **kwargs)
+        )
     return wrapper
