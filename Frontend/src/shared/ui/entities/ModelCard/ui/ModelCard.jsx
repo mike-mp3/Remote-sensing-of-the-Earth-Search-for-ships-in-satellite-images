6f267e3002cb8d2e59d3fd15c125c6a06fc36b86
CrazyInterface <your-second-email@example.com>
2025-05-25 02:41:36 +0300

    [*] refactoring of the structure model card

--- Frontend/src/shared/ui/entities/ModelCard/ui/ModelCard.jsx
+++ Frontend/src/shared/ui/entities/ModelCard/ui/ModelCard.jsx
@@ -15,13 +15,13 @@ const ModelCard = ({
         <div className={classes.card}>
             <div className={classes.imageContainer}>
                 <img src={url} alt="Model preview" className={classes.image} />
+                <div className={`${classes.statusOverlay} ${classes[status.toLowerCase()]}`}>
+                    {status.toUpperCase()}
+                </div>
             </div>
             <div className={classes.footer}>
                 <span className={classes.date}>
                     {new Date(created_at).toLocaleDateString()}
-                </span>
-                <span className={`${classes.status} ${classes[status.toLowerCase()]}`}>
-                    {status}
                 </span>
             </div>
         </div>
